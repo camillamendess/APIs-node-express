0868b4c441e4528849eb98e8557a22ea1c71797f
Camilla <milla_mendes83@hotmail.com>
2024-03-15 19:28:50 -0300

    delving deeper into express and routes

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-import http from "http";
+// import http from "http";
+
+import app from "./src/app.js"
 
 const PORT = 3000;
 
+/* 
+Maneira tradicional com módulo http (sem express) de criar o servidor e as rotas
+
 const rotas = {
-	"/": "Curso de Node.js",
-	"/livros": "Entrei na rota livros",
-	"/autores": "Entrei na rota autores"
+	"/": "<h1>Curso de Node.js</h1>",
+	"/livros": "<h1>Entrei na rota livros</h1>",
+	"/autores": "<h1>Entrei na rota autores</h1>"
 };
 
 const server = http.createServer((req, res) => {
 	res.writeHead(200, {
-		"Content-Type": "text/plain"
+		"Content-Type": "text/html"
 	});
 	res.end(rotas[req.url]);
-});
+}); */
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
 	console.log("Servidor escutando!");
 });
---
+++ src/app.js
@@ -0,0 +1,55 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id: 1,
+        titulo: "Harry Potter e a pedra filosofal"
+    },
+    {
+        id: 2,
+        titulo: "Jogos Vorazes "
+    }
+];
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+
+app.get("/", (req, res) => {
+    res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+// Pegar um livro com id específico 
+app.get("/livros/:id", (req, res) => {
+    // Recebe o numero do id com a função que pega o index do array de livros
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro removido com sucesso!");
+})
+
+export default app;
