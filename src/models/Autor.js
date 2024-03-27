a119e18bf8b7d56095fd50331cd681eaaa760866
Camilla <milla_mendes83@hotmail.com>
2024-03-26 22:16:11 -0300

    refactoring error handling

--- src/models/Autor.js
+++ src/models/Autor.js
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 const autorSchema = new mongoose.Schema(
   {
-    id: {type: String},
-    nome: {type: String, required: true},
-    nacionalidade: {type: String}
+    id: { type: String },
+    nome: {
+      type: String,
+      required: [true, "O nome do(a) autor(a) é obrigatório"],
+    },
+    nacionalidade: { type: String },
   },
   {
-    versionKey: false
+    versionKey: false,
   }
 );
 
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,15 +1,23 @@
 import mongoose from "mongoose";
 
-const livroSchema = new mongoose.Schema(
-  {
-    id: {type: String},
-    titulo: {type: String, required: true},
-    autor: {type: mongoose.Schema.Types.ObjectId, ref: "autores", required: true},
-    editora: {type: String, required: true},
-    numeroPaginas: {type: Number}
-  }
-);
+const livroSchema = new mongoose.Schema({
+  id: { type: String },
+  titulo: {
+    type: String,
+    required: [true, "O titulo do livro é obrigatório"],
+  },
+  autor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "autores",
+    required: [true, "O autor é obrigatório"],
+  },
+  editora: {
+    type: String,
+    required: [true, "A editora é obrigatoria"],
+  },
+  numeroPaginas: { type: Number },
+});
 
-const livros= mongoose.model("livros", livroSchema);
+const livros = mongoose.model("livros", livroSchema);
 
 export default livros;
